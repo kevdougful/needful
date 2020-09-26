5835fc36fff72bd8cd0c0abf440805c9925d2c45
Kevin Coffey <kevdougful@gmail.com>
2020-09-25 22:39:31 -0500

    Add Version Command

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "parse.h"
+#include "version.h"
 
 static char input[100] = "look around";
 
@@ -11,8 +13,13 @@ static bool getInput(void)
     return fgets(input, sizeof input, stdin) != NULL;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+    if (argc > 0 && strcmp(argv[1], "version") == 0)
+    {
+        printf("%s\n", VERSION);
+        return 0;
+    }
     printf("Hey! It's about time you showed up...\n");
     while (parseAndExecute(input) && getInput());
     printf("\nPfft.\n");
